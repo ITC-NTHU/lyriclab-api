5aea6e85edb01ef11a52c6fda4983ff37bee08e7
irina <syrok.BU@yandex.ru>
2024-10-07 19:11:42 +0800

    created lyrics api library

---
+++ lib/lyrics.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module GoodName
+  # provides lyrics
+  class Lyrics
+    def initialize(lyrics_data, data_source)
+      @lyrics = lyrics_data
+      @data_source = data_source
+    end
+
+    def lyrics
+      return @lyrics['plainLyrics'] if @lyrics && @lyrics['plainLyrics']
+
+      'Sorry... no lyrics found(('
+    end
+  end
+end
