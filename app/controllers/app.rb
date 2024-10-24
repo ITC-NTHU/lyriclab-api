1274e9c41b0f82559dd346b117efaa0754c8da83
irina <syrok.BU@yandex.ru>
2024-10-24 11:48:40 +0800

    updated app.rb

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,51 +2,56 @@
 
 require 'roda'
 require 'slim'
-require_relative '../models/entities/song'
-require_relative '../models/entities/lyrics'
-require_relative '../models/entities/album'
-require_relative '../models/entities/artist'
-require_relative '../models/gateways/lrclib_api'
-require_relative '../models/gateways/spotify_api'
 
 module LyricLab
   # Web App
   class App < Roda
     plugin :render, engine: 'slim', views: 'app/views'
-    plugin :assets, css: 'style.css', path: 'app/views/assets'
+    plugin :public, root: 'app/views/public'
+    plugin :assets, path: 'app/views/assets',
+                    css: 'style.css', js: 'table_row_click.js'
     plugin :common_logger, $stderr
     plugin :halt
-    plugin :flash
+    #plugin :flash
 
-    route do |current| # rubocop:disable Metrics/BlockLength
-      current.assets
+    route do |routing| # rubocop:disable Metrics/BlockLength
+      routing.assets # load CSS
 
       # GET /
-      current.root do
-        view 'home'
+      routing.root do
+        songs = Repository::For.klass(Entity::Song).all
+        view 'home', locals: { songs: }
       end
 
       # GET /search
-      current.on 'search' do
-        current.is do
+      routing.on 'search' do
+        routing.is do
           # POST /search/
-          current.post do
-            search_string = current.params['search_query']
-            current.redirect "/search/#{search_string}" if search_string
-            current.redirect '/'
+          routing.post do
+            search_string = routing.params['search_query']
+
+            # Get song info from APIs
+            song = Spotify::SongMapper
+                      .new(SP_CLIENT_ID, SP_CLIENT_SECRET)
+                      .find(search_string)
+
+            # Add song to database
+            Repository::For.entity(song).create(song)
+
+            # Redirect viewer to project page
+            routing.redirect "/search/#{song.title}/#{song.artist_name_string}" if search_string
           end
         end
 
         # GET /search/:query
-        current.on String do |query|
-          current.get do
-            song = Spotify::SongMapper
-                   .new(SP_CLIENT_ID, SP_CLIENT_SECRET)
-                   .find(query)
-            lyrics = Lrclib::LyricsMapper
-                     .new(song.title, song.artists.first.name)
-                     .search
-            view 'song', locals: { song:, lyrics: }
+        routing.on String, String do |title, artist_name|
+          routing.get do
+            # Get project from database
+            song = Repository::For.klass(Entity::Song)
+              .find_from_title_artist(title, artist_name)
+
+            # Show viewer the project
+            view 'song', locals: { song: }
           end
         end
       end
