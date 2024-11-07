7e41563eba9a99a8ccc1168b68fee16d34e38423
coffeecode1998 <coffeecode1998@gmail.com>
2024-11-07 12:47:16 +0800

    error check

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,25 +1,35 @@
 # frozen_string_literal: true
 
+require 'rack' # for Rack::MethodOverride
 require 'roda'
 require 'slim'
+require 'slim/include'
 
 module LyricLab
   # Web App
   class App < Roda
     #plugin :sessions, secret: config.SESSION_SECRET
-    plugin :render, engine: 'slim', views: 'app/views'
+    plugin :render, engine: 'slim', views: 'app/presentation/view_html'
     plugin :public, root: 'app/views/public'
     plugin :assets, path: 'app/views/assets',
                     css: 'style.css', js: 'table_row_click.js'
     plugin :common_logger, $stderr
     plugin :halt
-    # plugin :flash
+
+    use Rack::MethodOverride
 
     # Constants
     SPOTIFY_CLIENT_ID = LyricLab::App.config.SPOTIFY_CLIENT_ID
     SPOTIFY_CLIENT_SECRET = LyricLab::App.config.SPOTIFY_CLIENT_SECRET
     GOOGLE_CLIENT_KEY = LyricLab::App.config.GOOGLE_CLIENT_KEY
 
+    MESSAGES={
+      empty_search: 'Please enter a song name',
+      songs_not_found: 'Please search for another song',
+      # songs_exist : 'Songs already exist',
+      not_mandarin_songs: 'Please search for a Mandarin song',
+      no_lyrics_found: 'No lyrics found for this song'
+  }.freeze
 
     route do |routing|
       routing.assets # load CSS
@@ -36,22 +46,48 @@ module LyricLab
           # POST /search/
           routing.post do
             search_string = routing.params['search_query']
-            # Get song info from APIs
-            song = Spotify::SongMapper
-                   .new(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, GOOGLE_CLIENT_KEY)
-                   .find(search_string)
+           
+            unless !search_string.empty?
+              flash[:error] = MESSAGES[:empty_search]
+              response.status = 400
+              routing.redirect '/'
+            end
 
-            suggestions = []
-            suggestions = suggestions.append(song)    
-            suggestions = suggestions.append(song)
+            # Get song info from APIs
+            begin
+              song = Spotify::SongMapper
+                    .new(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, GOOGLE_CLIENT_KEY)
+                    .find(search_string)
+            rescue BadRequest  => e
+              flash[:error] = MESSAGES[:songs_not_found]
+            end
+
+            unless song
+              flash[:error] = MESSAGES[:songs_not_found]
+              routing.redirect '/'
+            end
+
+            unless song.lyrics.is_mandarin
+              flash[:error] = MESSAGES[:not_mandarin_songs]
+              routing.redirect '/'
+            end
+
+
+
+
+
+            # upload suggestion list to session
+            session[:suggestion] ||= []
+            session[:suggestion].append(song)
             session[:suggestions] = suggestions
 
             recommendation = Entity::Recommendation.new(song.title, song.artist_name_string, 1, song.spotify_id)
             Repository::For.entity(recommendation).create(recommendation)
+            
             # Add song to database if it doesn't already exist
             begin
               Repository::For.entity(song).create(song)
-            rescue StandardError => e
+            rescue StandardError => e # TODO: why error?
               # Handle the case where the song already exists
               puts "Error: #{e.message}"
             end
@@ -66,7 +102,7 @@ module LyricLab
         # GET /search
       routing.on 'show-suggestions' do
         routing.get do
-          s = session[:suggestions]
+          suggestions = session[:suggestions] || []
           #puts s
           view 'suggestion', locals: { suggestions: s}
         end 
@@ -77,6 +113,11 @@ module LyricLab
           routing.get do
             # Get song from database
             song = Repository::For.klass(Entity::Song).find_spotify_id(spotify_id)
+
+            unless song
+              flash[:error] = MESSAGES[:empty_search]
+              routing.redirect '/'
+            end
 
             # Show viewer the song
             view 'song', locals: { song: }
