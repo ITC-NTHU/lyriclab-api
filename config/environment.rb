eef1ea5bbca4b351f167fab71e35dc184fe9397e
Tristan <tristan.doering@tum.de>
2024-11-25 17:48:01 +0800

    initialize caching integration

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,8 @@ require 'figaro'
 require 'sequel'
 require 'rack/session'
 require 'logger'
-
+require 'rack/cache'
+require 'redis-rack-cache'
 module LyricLab
   # Environment specific configuration
   class App < Roda
@@ -20,6 +21,32 @@ module LyricLab
     Figaro.load
     def self.config = Figaro.env
 
+    configure :development, :production do
+      plugin :common_logger, $stderr
+    end
+
+    # Setup Cacheing mechanism
+    configure :development do
+      use Rack::Cache,
+          verbose: true,
+          metastore: 'file:_cache/rack/meta',
+          entitystore: 'file:_cache/rack/body'
+    end
+
+    configure :production do
+      use Rack::Cache,
+          verbose: true,
+          metastore: "#{config.REDISCLOUD_URL}/0/metastore",
+          entitystore: "#{config.REDISCLOUD_URL}/0/entitystore"
+    end
+
+    # Automated HTTP stubbing for testing only
+    configure :app_test do
+      require_relative '../spec/helpers/vcr_helper'
+      VcrHelper.setup_vcr
+      VcrHelper.configure_vcr_for_github(recording: :none)
+    end
+
     use Rack::Session::Cookie, secret: config.SESSION_SECRET
 
     configure :development, :test do
@@ -27,7 +54,7 @@ module LyricLab
       ENV['DATABASE_URL'] = "sqlite://#{config.DB_FILENAME}"
     end
 
-    # Database Setup
+    # Database Setup (ensure DATABASE_URL is already set on production)
     @db = Sequel.connect(ENV.fetch('DATABASE_URL'))
     def self.db = @db # rubocop:disable Style/TrivialAccessors
 
